6f990fe41d347172c6e6418f51ed7cbe377829ff
Annu Priya <113297237+Annu01042002@users.noreply.github.com>
2022-11-04 11:56:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import pickle
+import streamlit as st
+
 loaded_model= pickle.load(open("model.pkl", "rb"))
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
