ed937e079f025239a7871af420720afc56a4d6b4
Annu Priya <113297237+Annu01042002@users.noreply.github.com>
2022-11-04 11:48:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 import pickle
 loaded_model= pickle.load(open("model.pkl", "rb"))
 pred = st.file_uploader("upload csv files")
