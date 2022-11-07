1ca0144ea3680bbfd8900b3439a4ee78fb436cc9
Annu Priya <113297237+Annu01042002@users.noreply.github.com>
2022-11-07 23:53:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import numpy as np
-import pandas as pd
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import pickle
 import streamlit as st
-import base64
-import io
-import os
+import pandas as pd
 
 # DB Management
 import sqlite3 
@@ -26,6 +17,12 @@ def check_hashes(password,hashed_text):
 	if make_hashes(password) == hashed_text:
 		return hashed_text
 	return False
+
+
+
+
+
+
 def create_usertable():
 	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
@@ -43,42 +40,14 @@ def login_user(username,password):
 def view_all_users():
 	c.execute('SELECT * FROM userstable')
 	data = c.fetchall()
-	return 
-
-#background image
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-def credit_detection():
-	st.title("Credit Card Fraud Detection System")
-	st.caption("Upload Your Data to Detect the Fraudulent Transaction") 
-	loaded_model= pickle.load(open("model.pkl", "rb"))
-	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-	# dataframe = pd.read_csv(uploaded_files,  encoding='utf-8')
-	# st.write(dataframe)
-	submit = st.button("Submit")
-	if submit:
-		st.sucess("Ok")
+	return data
 
 
 
 def main():
-	
-	add_bg_from_local('asset/milad-fakurian-E8Ufcyxz514-unsplash.jpg')
+	"""Simple Login App"""
 
-	st.title("Credit Card Fraud Detection System")
-	st.header("Welcome To our Website")
+	st.title("Simple Login App")
 
 	menu = ["Home","Login","SignUp"]
 	choice = st.sidebar.selectbox("Menu",menu)
@@ -112,9 +81,12 @@ def main():
 					user_result = view_all_users()
 					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
 					st.dataframe(clean_db)
-	
 			else:
 				st.warning("Incorrect Username/Password")
+
+
+
+
 
 	elif choice == "SignUp":
 		st.subheader("Create New Account")
@@ -129,19 +101,62 @@ def main():
 
 
 
+
+
+
+
+
 if __name__ == '__main__':
 	main()
 
 
-st.title("Credit Card Fraud Detection System")
-st.caption("Upload Your Data to Detect the Fraudulent Transaction") 
-loaded_model= pickle.load(open("model.pkl", "rb"))
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-# dataframe = pd.read_csv(uploaded_files,  encoding='utf-8')
-# st.write(dataframe)
-submit = st.button("Submit")
-if submit:
-    st.sucess("Ok")
+
+# import numpy as np
+# import pandas as pd
+# from sklearn.metrics import confusion_matrix
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import accuracy_score
+# import pickle
+# import streamlit as st
+# import base64
+# import io
+# import os
+
+# # DB Management
+# import sqlite3 
+# conn = sqlite3.connect('data.db')
+# c = conn.cursor()
+
+
+# # Security
+# #passlib,hashlib,bcrypt,scrypt
+# import hashlib
+# def make_hashes(password):
+# 	return hashlib.sha256(str.encode(password)).hexdigest()
+
+# def check_hashes(password,hashed_text):
+# 	if make_hashes(password) == hashed_text:
+# 		return hashed_text
+# 	return False
+# def create_usertable():
+# 	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+# def add_userdata(username,password):
+# 	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+# 	conn.commit()
+
+# def login_user(username,password):
+# 	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+# 	data = c.fetchall()
+# 	return data
+
+
+# def view_all_users():
+# 	c.execute('SELECT * FROM userstable')
+# 	data = c.fetchall()
+# 	return 
 
 # #background image
 # def add_bg_from_local(image_file):
@@ -158,4 +173,102 @@ if submit:
 #     """,
 #     unsafe_allow_html=True
 #     )
-# add_bg_from_local('asset/milad-fakurian-E8Ufcyxz514-unsplash.jpg')
+# def credit_detection():
+# 	st.title("Credit Card Fraud Detection System")
+# 	st.caption("Upload Your Data to Detect the Fraudulent Transaction") 
+# 	loaded_model= pickle.load(open("model.pkl", "rb"))
+# 	uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# 	# dataframe = pd.read_csv(uploaded_files,  encoding='utf-8')
+# 	# st.write(dataframe)
+# 	submit = st.button("Submit")
+# 	if submit:
+# 		st.sucess("Ok")
+
+
+
+# def main():
+	
+# 	add_bg_from_local('asset/milad-fakurian-E8Ufcyxz514-unsplash.jpg')
+
+# 	st.title("Credit Card Fraud Detection System")
+# 	st.header("Welcome To our Website")
+
+# 	menu = ["Home","Login","SignUp"]
+# 	choice = st.sidebar.selectbox("Menu",menu)
+
+# 	if choice == "Home":
+# 		st.subheader("Home")
+
+# 	elif choice == "Login":
+# 		st.subheader("Login Section")
+
+# 		username = st.sidebar.text_input("User Name")
+# 		password = st.sidebar.text_input("Password",type='password')
+# 		if st.sidebar.checkbox("Login"):
+# 			# if password == '12345':
+# 			create_usertable()
+# 			hashed_pswd = make_hashes(password)
+
+# 			result = login_user(username,check_hashes(password,hashed_pswd))
+# 			if result:
+
+# 				st.success("Logged In as {}".format(username))
+
+# 				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
+# 				if task == "Add Post":
+# 					st.subheader("Add Your Post")
+
+# 				elif task == "Analytics":
+# 					st.subheader("Analytics")
+# 				elif task == "Profiles":
+# 					st.subheader("User Profiles")
+# 					user_result = view_all_users()
+# 					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+# 					st.dataframe(clean_db)
+	
+# 			else:
+# 				st.warning("Incorrect Username/Password")
+
+# 	elif choice == "SignUp":
+# 		st.subheader("Create New Account")
+# 		new_user = st.text_input("Username")
+# 		new_password = st.text_input("Password",type='password')
+
+# 		if st.button("Signup"):
+# 			create_usertable()
+# 			add_userdata(new_user,make_hashes(new_password))
+# 			st.success("You have successfully created a valid Account")
+# 			st.info("Go to Login Menu to login")
+
+
+
+# if __name__ == '__main__':
+# 	main()
+
+
+# st.title("Credit Card Fraud Detection System")
+# st.caption("Upload Your Data to Detect the Fraudulent Transaction") 
+# loaded_model= pickle.load(open("model.pkl", "rb"))
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# # dataframe = pd.read_csv(uploaded_files,  encoding='utf-8')
+# # st.write(dataframe)
+# submit = st.button("Submit")
+# if submit:
+#     st.sucess("Ok")
+
+# # #background image
+# # def add_bg_from_local(image_file):
+# #     with open(image_file, "rb") as image_file:
+# #         encoded_string = base64.b64encode(image_file.read())
+# #     st.markdown(
+# #     f"""
+# #     <style>
+# #     .stApp {{
+# #         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+# #         background-size: cover
+# #     }}
+# #     </style>
+# #     """,
+# #     unsafe_allow_html=True
+# #     )
+# # add_bg_from_local('asset/milad-fakurian-E8Ufcyxz514-unsplash.jpg')
