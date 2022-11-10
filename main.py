891529520816bc02942c626b89ce396107fcef80
Annu Priya <113297237+Annu01042002@users.noreply.github.com>
2022-11-10 21:02:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import scikit-learn as sklearn
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
